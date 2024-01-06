2a7996f274562abe009c25439297bdd54b87c114
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2024-01-06 16:06:40 +0530

    Update Graph.js

--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -1,51 +1,52 @@
-import React from 'react'
-import{
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+// Import React and Chart.js components for creating and rendering graphs
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import { useTheme } from '../Context/ThemeContext';
+
+// Register necessary Chart.js components
 ChartJS.register(
-    
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-)
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-const Graph = ({graphData}) => {
+// Graph component definition
+const Graph = ({ graphData }) => {
+  // Access the current theme using the useTheme hook
+  const { theme } = useTheme();
 
-const {theme} = useTheme();
-
+  // Render the Line chart with the provided graph data
   return (
     <div>
-        <Line 
-            data={
-                {
-                    labels: graphData.map(i=>i[0]),
-                    datasets: [
-                       {
-                        data: graphData.map(i=>i[1]),
-                        label: 'wpm',
-                        borderColor: theme.textColor
-                       }
-                       
-                    ]
-                }
-            }
-        />
+      <Line
+        data={{
+          labels: graphData.map((i) => i[0]),
+          datasets: [
+            {
+              data: graphData.map((i) => i[1]),
+              label: 'wpm',
+              borderColor: theme.textColor,
+            },
+          ],
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
+// Export the Graph component
 export default Graph;
-
