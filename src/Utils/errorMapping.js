e5d5ef9acc0c7c67ce4ff61635e7a5687cbed013
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2024-01-06 16:16:34 +0530

    Update errorMapping.js

--- src/Utils/errorMapping.js
+++ src/Utils/errorMapping.js
@@ -1,11 +1,11 @@
+// Mapping of Firebase authentication error codes to user-friendly error messages
 const errorMapping = {
-    'auth/user-not-found' : "Please enter a valid email address or signup",
-    'auth/wrong-password' : "Incorrect credentials",
-    'auth/email-already-in-use' : "Email already in use",
+    'auth/user-not-found': "Please enter a valid email address or sign up",
+    'auth/wrong-password': "Incorrect credentials",
+    'auth/email-already-in-use': "Email already in use",
     'auth/weak-password': "Password must be 6 or more characters long",
-    'auth/unknown' : "Please try again later"
+    'auth/unknown': "Please try again later"
+};
 
-}
+// Exporting the errorMapping constant for use in other parts of the application
 export default errorMapping;
-
-
