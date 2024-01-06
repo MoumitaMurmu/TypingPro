18a68cca0e46a577baddbad22e5073ec056e6513
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2024-01-06 16:08:07 +0530

    Update LoginForm.js

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,109 +1,122 @@
 import { Box, Button, TextField } from '@mui/material';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useTheme } from '../Context/ThemeContext';
 import { auth } from '../firebaseConfig';
 import { toast } from 'react-toastify';
 import errorMapping from '../Utils/errorMapping';
 
-const LoginForm = ({handleClose}) => {
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
-    const {theme} = useTheme();
+const LoginForm = ({ handleClose }) => {
+    // State variables for managing email and password
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleSubmit = () =>{
-        if(!email || !password){
+    // Function to handle form submission
+    const handleSubmit = () => {
+        // Check if email and password are provided
+        if (!email || !password) {
             toast.warning('Fill all details!', {
-                position: "top-right",
+                position: 'top-right',
                 autoClose: 5000,
                 hideProgressBar: false,
                 closeOnClick: true,
                 pauseOnHover: true,
                 draggable: true,
                 progress: undefined,
-                theme: "dark",
-                });
-           return;
+                theme: 'dark',
+            });
+            return;
         }
 
-        auth.signInWithEmailAndPassword(email,password).then((res)=>{
-            toast.success('Logged In!', {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "dark",
+        // Sign in with email and password using Firebase authentication
+        auth.signInWithEmailAndPassword(email, password)
+            .then((res) => {
+                // Display success message and close the modal
+                toast.success('Logged In!', {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'dark',
                 });
                 handleClose();
-        }).catch((err)=>{
-            toast.error(errorMapping[err.code] || 'some error occured', {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "dark",
+            })
+            .catch((err) => {
+                // Display error message or a generic message if the error code is not mapped
+                toast.error(errorMapping[err.code] || 'Some error occurred', {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'dark',
                 });
-        })
-    }
+            });
+    };
 
-  return (
-    <div>
-        <Box
-        p={3}
-        style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '20px'
-        }}
-        >
-            <TextField
-                variant='outlined'
-                type='email'
-                label='Enter Your Email'
-                onChange={(e)=>setEmail(e.target.value)}
-                InputLabelProps={{
-                    style : {
-                      color: 'black'
-                    }
+    return (
+        <div>
+            {/* Form Container */}
+            <Box
+                p={3}
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '20px',
                 }}
-                inputProps={{
-                    style: {
-                        color: 'black'
-                    }
-                }}
-            />
-            <TextField
-                variant='outlined'
-                type='password'
-                label='Enter Your Password'
-                onChange={(e)=>setPassword(e.target.value)}
-                InputLabelProps={{
-                    style : {
-                      color: 'black'
-                    }
-                }}
-                inputProps={{
-                    style: {
-                        color: 'black'
-                    }
-                }}
-            />
-            <Button
-            variant='contained'
-            size='large'
-            className='sign-up-btn'
-            onClick={handleSubmit}>Login</Button>
-        </Box>
-    </div>
-  )
-}
+            >
+                {/* Email Input */}
+                <TextField
+                    variant='outlined'
+                    type='email'
+                    label='Enter Your Email'
+                    onChange={(e) => setEmail(e.target.value)}
+                    InputLabelProps={{
+                        style: {
+                            color: 'black',
+                        },
+                    }}
+                    inputProps={{
+                        style: {
+                            color: 'black',
+                        },
+                    }}
+                />
+
+                {/* Password Input */}
+                <TextField
+                    variant='outlined'
+                    type='password'
+                    label='Enter Your Password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    InputLabelProps={{
+                        style: {
+                            color: 'black',
+                        },
+                    }}
+                    inputProps={{
+                        style: {
+                            color: 'black',
+                        },
+                    }}
+                />
+
+                {/* Login Button */}
+                <Button
+                    variant='contained'
+                    size='large'
+                    className='sign-up-btn'
+                    onClick={handleSubmit}
+                >
+                    Login
+                </Button>
+            </Box>
+        </div>
+    );
+};
 
 export default LoginForm;
-
-
-            
