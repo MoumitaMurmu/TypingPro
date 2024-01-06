52c9378b60a56fa8d57bd116428784c2bfa0dc0c
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2024-01-06 16:13:33 +0530

    Update HomePage.js

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,30 +1,27 @@
-import React from 'react'
+import React from 'react';
 import Header from '../Components/Header';
 import TypingBox from '../Components/TypingBox';
 import Footer from '../Components/Footer';
 
-
-
 const HomePage = () => {
   return (
-    <div >
     <div>
-    <Header/>
-    </div>
-    
-    <div className="canvas">
-    
-    <TypingBox/>
-  
-    </div>
-  <div>
-  <Footer/>
-  </div>
-      
-     
+      {/* Header component for the top section of the page */}
+      <div>
+        <Header />
       </div>
-  )
+      
+      {/* Main content area for the TypingBox */}
+      <div className="canvas">
+        <TypingBox />
+      </div>
+
+      {/* Footer component for the bottom section of the page */}
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
-
