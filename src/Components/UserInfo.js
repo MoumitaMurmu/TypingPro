6ab5b79aad41d39caf53c1ea8b830051cc3016f5
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2024-01-06 16:11:20 +0530

    Update UserInfo.js

--- src/Components/UserInfo.js
+++ src/Components/UserInfo.js
@@ -1,48 +1,41 @@
-import React from 'react'
+import React from 'react';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebaseConfig';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import { Button } from '@mui/material';
-import {SlBadge} from 'react-icons/sl';
+import { SlBadge } from 'react-icons/sl';
 
-const UserInfo = ({totalTestsTaken}) => {
-    const [user] = useAuthState(auth);
-   
+const UserInfo = ({ totalTestsTaken }) => {
+  // Get the authenticated user's information
+  const [user] = useAuthState(auth);
 
-
-    
   return (
     <>
-    <div className='user-profile'>
+      <div className='user-profile'>
         <div className='user'>
-            <div className='picture'>
-                <AccountCircleIcon style={{display: 'block', transform: 'scale(6)', margin: 'auto', marginTop: '2rem'}}/>
+          <div className='picture'>
+            {/* Display user profile picture (placeholder with AccountCircleIcon) */}
+            <AccountCircleIcon style={{ display: 'block', transform: 'scale(6)', margin: 'auto', marginTop: '2rem' }} />
+          </div>
+          <div className='info'>
+            {/* Display user email and account creation time */}
+            <div className='email'>
+              <MailOutlineIcon style={{ marginRight: '5px', fontSize: '30px' }} /> {user.email}
             </div>
-            <div className='info'>
-                <div className='email'>
-                   <MailOutlineIcon style={{marginRight: '5px', fontSize:'30px'}}/>{user.email}
-                </div>
-                <div className='joined-at'>
-                   {user.metadata.creationTime}
-                </div>
+            <div className='joined-at'>
+              {user.metadata.creationTime}
             </div>
-
+          </div>
         </div>
+
         <div className='total-tests'>
-        <span><SlBadge style={{ marginLeft: '2rem'}}/></span>
-
-             <span>Total Test Taken - {totalTestsTaken}</span>
-
+          {/* Display a badge icon and the total number of tests taken */}
+          <span><SlBadge style={{ marginLeft: '2rem' }} /></span>
+          <span>Total Tests Taken - {totalTestsTaken}</span>
         </div>
-       
-
-    </div>
-</>
-  )
+      </div>
+    </>
+  );
 }
 
 export default UserInfo;
-
-
-       
