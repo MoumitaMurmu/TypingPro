744d02de834ad5792f780aeb56dc252b0228cc11
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2024-01-06 16:18:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Import necessary modules and components from React and external libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -5,23 +6,22 @@ import { TestModeContextProvider } from './Context/TestModeContext';
 import { ThemeContextProvider } from './Context/ThemeContext';
 import { BrowserRouter } from 'react-router-dom';
 
+// Create a root for ReactDOM to render the application into the 'root' element in the HTML
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the application with React StrictMode enabled
 root.render(
   <React.StrictMode>
-  <ThemeContextProvider>
-    <TestModeContextProvider>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    </TestModeContextProvider>
-  </ThemeContextProvider>
- 
-  
+    {/* Provide the theme context to the entire application */}
+    <ThemeContextProvider>
+      {/* Provide the test mode context to the entire application */}
+      <TestModeContextProvider>
+        {/* Enable client-side routing with BrowserRouter */}
+        <BrowserRouter>
+          {/* Render the main application component */}
+          <App />
+        </BrowserRouter>
+      </TestModeContextProvider>
+    </ThemeContextProvider>
   </React.StrictMode>
 );
-
-
-
-
-
-
